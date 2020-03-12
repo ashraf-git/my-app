adca06ee297535032b9a705a574857bc008284d2
ashraf-git <aliashraf.hena@gmail.com>
2020-03-12 13:02:09 +0600

    Pass dynamic data to components

--- src/App.js
+++ src/App.js
@@ -3,27 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const noyoks = ["aliashraf","hena begum", "ali", "ashraf"]
 return (
     <div className="App">
       <header className="App-header">
         <h1>I am React Lover.</h1>
-        <Person></Person>
+        <Person name={noyoks[0]} lover = 'Mossammad Hena Begum'></Person>
+        <Person name="Mossammad Hena Begum" lover='Mohammad Ali Ashraf Khan'></Person>
         <Person></Person>
       </header>
     </div>
   );
 }
 
-function Person(){
+function Person(props){
   const personStyle = {
     color: 'yellow',
     border: '2px solid green',
     margin: '10px',
     padding: '10px'
   }
+  console.log(props)
   return <div style = {personStyle}>
-  <h1> Name: Shakib Al Hasan</h1>
-  <h3>hero of the year</h3>
+  <h1> Name: {props.name}</h1>
+  <h3>hero of {props.lover}</h3>
   </div>
 }
 
